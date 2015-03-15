1e2931fdf37f25dab5b1dba340922e4e753424be
wjhssfs <wjhssfs@gmail.com>
2015-03-15 12:18:33 -0700

    Add Number of 1 Bits

---
+++ 191Number_of_1_Bits.hpp
@@ -0,0 +1,18 @@
+// 191 Number of 1 Bits 
+// 
+// Write a function that takes an unsigned integer and returns the number of ’1' bits it has (also known as the Hamming weight).
+// 
+// For example, the 32-bit integer ’11' has binary representation 00000000000000000000000000001011, so the function should return 3.
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int w = 0;
+        while (n)
+        {
+            w += n & 1;
+            n >>= 1;
+        }
+        return w;
+    }
+};
