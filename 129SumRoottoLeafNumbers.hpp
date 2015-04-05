c2e04250b0dbc5e8b109b9e03d9bfda1c36a1f1d
wjhssfs <wjhssfs@gmail.com>
2015-04-05 09:41:12 -0700

    129

---
+++ 129SumRoottoLeafNumbers.hpp
@@ -0,0 +1,45 @@
+// 128 Sum Root to Leaf Numbers 
+// Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+// 
+// An example is the root-to-leaf path 1->2->3 which represents the number 123.
+// 
+// Find the total sum of all root-to-leaf numbers.
+// 
+// For example,
+// 
+//     1
+//    / \
+//   2   3
+// The root-to-leaf path 1->2 represents the number 12.
+// The root-to-leaf path 1->3 represents the number 13.
+// 
+// Return the sum = 12 + 13 = 25.
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int sumNumbers(TreeNode *root) {
+        if(!root) return 0;
+        int total = 0;
+        dfs(root, 0, total);
+        return total;
+    }
+    
+    void dfs(TreeNode *root, int pVal, int &total){
+        if(root->left){
+            dfs(root->left, pVal*10+root->val, total);
+        }
+        if(root->right){
+            dfs(root->right, pVal*10+root->val, total);
+        }
+        if(!root->left && !root->right) total+=pVal*10+root->val;
+    }
+};
