66cc9c3a624032fbc30cea0d4150c04a30c7e996
wjhssfs <wjhssfs@gmail.com>
2015-05-25 11:50:44 -0700

    Update 148

--- 148SortList.hpp
+++ 148SortList.hpp
@@ -19,6 +19,39 @@ public:
         return sortLinkedList(head, getLength(head));
     }
     
+    // Iterative, const space
+   	ListNode *sortListIterative(ListNode *head) {
+		ListNode dummy{ 0 };
+		dummy.next = head;
+
+		int len = getLength(head), mergeSize = 1;
+		while (mergeSize < len) {
+			ListNode *preHead1 = &dummy;
+			while (preHead1->next){
+				ListNode *tail1 = preHead1, *tail2 = preHead1; //tail1 is also the preHead2
+				int i = mergeSize;
+				while (i-- && tail1->next) {
+					tail1 = tail1->next;
+					// Move ahead twice if possible
+					if (tail2->next) tail2 = tail2->next;
+					if (tail2->next) tail2 = tail2->next;
+				}
+				if (!tail1->next) break; // list 2 doesn't exist, break early
+
+				// Split into different list
+				ListNode *head1 = preHead1->next, *head2 = tail1->next, *nextNode = tail2->next;
+				preHead1->next = tail1->next = tail2->next = nullptr;
+				preHead1->next = mergeList(head1, head2);
+				// After merge, connect all lists into one.
+				preHead1 = tail1->val <= tail2->val ? tail2 : tail1; // mergeList below is stable. It makes sure for equal items, item in l1 comes first
+				preHead1->next = nextNode;
+			}
+			mergeSize *= 2;
+		}
+		return dummy.next;;
+	}
+
+	// Recursive
     ListNode* sortLinkedList(ListNode *&head, int N) {
         if (N == 0) return NULL;
         if (N == 1) {
@@ -38,7 +71,7 @@ public:
         ListNode *cur = &dummy;
         while (head1 && head2)
         {
-            ListNode **min = head1->val < head2->val ? &head1 : &head2;
+            ListNode **min = head1->val <= head2->val ? &head1 : &head2;
             cur->next = *min;
             cur = cur->next;
             *min = (*min)->next;
