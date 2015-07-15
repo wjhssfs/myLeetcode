60f7f2209c14afbda830d1c90073066cf4c1645b
wjhssfs <wjhssfs@gmail.com>
2015-07-15 10:49:39 -0700

    224-227

--- 224BasicCalculator.hpp
+++ 224BasicCalculator.hpp
@@ -11,6 +11,36 @@
 // "(1+(4+5+2)-3)+(6+8)" = 23
 
 
+// Example: 7 - ( 6 - 5 - ( 4 - 3 ) - 2 ) - ( 1 )
+// Result: + 7 - 6 + 5 + 4 - 3 + 2 - 1
+// The bold + - are signs just before '('. They are dealt with previous stack sign. They are saved in the stack to help decide the signs in the following parentheses
+class Solution {
+public:
+    int calculate(string s) {
+        stack<int> t;  //previous sign just before the '('
+        t.push(1);  //for all the signs those are not in the parentheses
+        int sum=0, temp=0, lastSign=1;
+        for(auto c: s)
+        {
+            if(c=='(') t.push(lastSign); //save the sign just before the '('
+            else if(c==')') t.pop();
+
+            if(c>='0' && c<='9'){  
+                temp=temp*10+c-'0';
+            }
+
+            if(c=='-'||c=='+')
+            {
+                sum+=lastSign*temp; //calculate the number before current sign
+                temp=0;
+                lastSign=(c=='-'?-1:1)*t.top();  //deal with the stack influencing sign
+            }
+        }
+        sum+=lastSign*temp; //calculate the last number
+        return sum;
+    }
+};
+
 class Solution {
 public:
     enum TokenType{
---
+++ 227BasicCalculatorII.hpp
@@ -0,0 +1,97 @@
+// 227 Basic Calculator II
+
+// Implement a basic calculator to evaluate a simple expression string.
+
+// The expression string contains only non-negative integers, +, -, *, / operators and empty spaces . The integer division should truncate toward zero.
+
+// You may assume that the given expression is always valid.
+
+// Some examples:
+// "3+2*2" = 7
+// " 3/2 " = 1
+// " 3+5 / 2 " = 5
+
+
+class Solution {
+public:
+    int calculate(string s) {
+        int result = 0, cur_res = 0;
+        char op = '+';
+        for(int pos = s.find_first_not_of(' '); pos < s.size(); pos = s.find_first_not_of(' ', pos)) {
+            if(isdigit(s[pos])) {
+                int tmp = s[pos] - '0';
+                while(++pos < s.size() && isdigit(s[pos]))
+                    tmp = tmp*10 + (s[pos] - '0');
+                switch(op) {
+                    case '+' : cur_res += tmp; break;
+                    case '-' : cur_res -= tmp; break;
+                    case '*' : cur_res *= tmp; break;
+                    case '/' : cur_res /= tmp; break;
+                }
+            }
+            else {
+                if(s[pos] == '+' || s[pos] == '-') {
+                    result += cur_res;
+                    cur_res = 0;
+                }
+                op = s[pos++];
+            }
+        }
+        return result + cur_res;
+    }
+};
+
+class Solution {
+    stack<int> nums;
+    stack<char> ops;
+public:
+    int calculate(string s) {
+        if (s.empty()) return 0;
+        int index = 0, value = 0;
+        char t = match(s, index, value);
+        while (ops.empty() || ops.top()!='$'){
+            if (t == '#'){
+                nums.push(value);
+            }
+            else if (ops.empty() || pri(ops.top()) < pri(t)){
+                ops.push(t);
+            }
+            else{
+                while (!ops.empty() && pri(ops.top()) >= pri(t)){
+                    int op2 = nums.top(); nums.pop();
+                    int op1 = nums.top(); nums.pop();
+                    if (ops.top() == '+') nums.push(op1 + op2);
+                    if (ops.top() == '-') nums.push(op1 - op2);
+                    if (ops.top() == '*') nums.push(op1 * op2);
+                    if (ops.top() == '/') nums.push(op1 / op2);
+                    ops.pop();
+                }
+                ops.push(t);
+            }
+            t = match(s, index, value);
+        }
+
+        return nums.top();
+    }
+
+    int pri(char c){
+        if (c == '$') return 0;
+        if (c == '+' || c == '-') return 1;
+        if (c == '*' || c == '/') return 2;
+    }
+
+    char match(string &s, int &index, int &value){
+        while (s[index] == ' ') ++index;
+        if (index == s.size()) return '$';
+        if (isdigit(s[index])){
+            value = 0;
+            while (index < s.size() && isdigit(s[index])){
+                value *= 10;
+                value += s[index] - '0';
+                ++index;
+            }
+            return '#';
+        }
+        return s[index++];
+    }
+};
