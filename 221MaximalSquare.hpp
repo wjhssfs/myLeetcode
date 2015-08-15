0e66267ce803c24b30c2159ab10a5932e05e8ba5
wjhssfs <wjhssfs@gmail.com>
2015-08-15 13:18:47 -0700

    r 221

--- 221MaximalSquare.hpp
+++ 221MaximalSquare.hpp
@@ -21,7 +21,7 @@ public:
         for(int i = 0; i < rows; i++){
             for(int j = 0; j < cols; j++){
                 m[i][j] = matrix[i][j]=='0'? make_pair(0,0) :
-                make_pair(i==0?1:m[i-1][j].first + 1, j==0?1:m[i][j-1].second + 1);
+                    make_pair(i==0?1:m[i-1][j].first + 1, j==0?1:m[i][j-1].second + 1);
                 int curMaxLength = maxSquareLen(m, i, j);
                 res = max(res, curMaxLength*curMaxLength);
             }
@@ -42,3 +42,28 @@ public:
         return layer;
     }
 };	
+
+
+public int maximalSquare(char[][] a) {
+  if (a == null || a.length == 0 || a[0].length == 0)
+    return 0;
+
+  int max = 0, n = a.length, m = a[0].length;
+
+  // dp(i, j) represents the length of the square 
+  // whose lower-right corner is located at (i, j)
+  // dp(i, j) = min{ dp(i-1, j-1), dp(i-1, j), dp(i, j-1) }
+  int[][] dp = new int[n + 1][m + 1];
+
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= m; j++) {
+      if (a[i - 1][j - 1] == '1') {
+        dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+        max = Math.max(max, dp[i][j]);
+      }
+    }
+  }
+
+  // return the area
+  return max * max;
+}
--- 223RectangleArea.hpp
+++ 223RectangleArea.hpp
@@ -8,6 +8,7 @@
 
 class Solution {
 public:
+	// (A,B) left bottom cordinate. (C,D) up right condinate.
     int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
         int total = (C-A)*(D-B) + (G-E)*(H-F);
         if(A>G||E>C||B>H||F>D) return total;
