8f76e1b9fe96ac4747a34909fa1a9f672685b72a
wjhssfs <wjhssfs@gmail.com>
2015-11-25 13:35:41 -0800

    258

---
+++ 258AddDigits.hpp
@@ -0,0 +1,28 @@
+// 258 Add Digits 
+
+// Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+// For example:
+
+// Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+
+// Follow up:
+// Could you do it without any loop/recursion in O(1) runtime?
+
+// Hint:
+
+// A naive implementation of the above process is trivial. Could you come up with other methods?
+// What are all the possible results?
+// How do they occur, periodically or randomly?
+// You may find this Wikipedia article useful.
+// https://en.wikipedia.org/wiki/Digital_root
+
+
+class Solution {
+public:
+    int addDigits(int num) {
+        if(!num) return 0;
+        if(!(num%9)) return 9;
+        else return num%9;
+    }
+};
