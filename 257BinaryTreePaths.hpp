ec98498332b3a49f3289c5196cf1b35fa6fb58ac
wjhssfs <wjhssfs@gmail.com>
2015-11-25 12:43:21 -0800

    257

---
+++ 257BinaryTreePaths.hpp
@@ -0,0 +1,45 @@
+// 257 Binary Tree Paths
+
+// Given a binary tree, return all root-to-leaf paths.
+
+// For example, given the following binary tree:
+
+//    1
+//  /   \
+// 2     3
+//  \
+//   5
+// All root-to-leaf paths are:
+
+// ["1->2->5", "1->3"]
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        if(!root) return result;
+        buildTreePaths(root, "");
+        return result;
+    }
+    
+    void buildTreePaths(TreeNode *root, string curPath) {
+        curPath += "->" + to_string(root->val);
+        if(!root->left && !root->right){
+            result.push_back(curPath.substr(2));
+            return;
+        }
+        if(root->left) buildTreePaths(root->left, curPath);
+        if(root->right) buildTreePaths(root->right, curPath);
+    }
+    
+    vector<string> result;
+};
