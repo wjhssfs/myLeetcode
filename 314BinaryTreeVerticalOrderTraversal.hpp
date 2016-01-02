70b50b6c3ca5327a117fe99cda225dc008624b59
wjhssfs <wjhssfs@gmail.com>
2016-01-02 09:49:51 -0800

    314

--- 314BinaryTreeVerticalOrderTraversal.hpp
+++ 314BinaryTreeVerticalOrderTraversal.hpp
@@ -42,6 +42,28 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+ vector<vector<int>> verticalOrder(TreeNode* root) {
+    map<int, vector<int>> cols;
+    queue<pair<TreeNode*, int>> q;
+    if (root)
+        q.emplace(root, 0);
+    while (q.size()) {
+        auto node = q.front().first;
+        int x = q.front().second;
+        q.pop();
+        cols[x].push_back(node->val);
+        if (node->left)
+            q.emplace(node->left, x-1);
+        if (node->right)
+            q.emplace(node->right, x+1);
+    }
+    vector<vector<int>> result;
+    for (auto col : cols)
+        result.push_back(col.second);
+    return result;
+}
+
 class Solution {
 public:
     vector<vector<int>> verticalOrder(TreeNode* root) {
