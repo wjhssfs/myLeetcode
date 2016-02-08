a7f57e3d8ed121f1c94c332926c143a185ab6bb4
wjhssfs <wjhssfs@gmail.com>
2016-02-07 20:18:27 -0800

    update

--- 328OddEvenLinkedList.hpp
+++ 328OddEvenLinkedList.hpp
@@ -46,3 +46,18 @@ public:
         return head;
     }
 };
+
+  ListNode* oddEvenList(ListNode* head) 
+    {
+        if(!head) return head;
+        ListNode *odd=head, *evenhead=head->next, *even = evenhead;
+        while(even && even->next)
+        {
+            odd->next = odd->next->next;
+            even->next = even->next->next;
+            odd = odd->next;
+            even = even->next;
+        }
+        odd->next = evenhead;
+        return head;
+    }
--- 329LongestIncreasingPathInAMatrix.hpp
+++ 329LongestIncreasingPathInAMatrix.hpp
@@ -45,7 +45,7 @@ private:
     void dfs(vector<vector<int>>& matrix, int r, int c, vector<vector<int>>& maxLenTable) {
         if (maxLenTable[r][c] != 0)
             return;
-        maxLenTable[r][c] = -1;
+        maxLenTable[r][c] = -1; // This is not needed, since previous node in the path will be filtered out by check matrix[r][c] >= matrix[nR][nC])
         int m = (int)matrix.size();
         int n = (int)matrix[0].size();
         int maxLenNext = 0;
@@ -65,3 +65,34 @@ private:
         return;
     }
 };
+
+
+
+public static final int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+public int longestIncreasingPath(int[][] matrix) {
+    if(matrix.length == 0) return 0;
+    int m = matrix.length, n = matrix[0].length;
+    int[][] cache = new int[m][n];
+    int max = 1;
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            int len = dfs(matrix, i, j, m, n, cache);
+            max = Math.max(max, len);
+        }
+    }   
+    return max;
+}
+
+public int dfs(int[][] matrix, int i, int j, int m, int n, int[][] cache) {
+    if(cache[i][j] != 0) return cache[i][j];
+    int max = 1;
+    for(int[] dir: dirs) {
+        int x = i + dir[0], y = j + dir[1];
+        if(x < 0 || x >= m || y < 0 || y >= n || matrix[x][y] <= matrix[i][j]) continue;
+        int len = 1 + dfs(matrix, x, y, m, n, cache);
+        max = Math.max(max, len);
+    }
+    cache[i][j] = max;
+    return max;
+}
