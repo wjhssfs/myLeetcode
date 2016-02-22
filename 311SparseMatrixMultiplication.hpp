3496d10f3439540ac466ddc5fa71a7f77fd9dd6f
wjhssfs <wjhssfs@gmail.com>
2016-02-21 16:19:02 -0800

    update

--- 311SparseMatrixMultiplication.hpp
+++ 311SparseMatrixMultiplication.hpp
@@ -70,3 +70,29 @@ public:
 		return r;
 	}
 };
+
+class Solution {
+public:
+    vector<vector<int>> multiply(vector<vector<int>>& A, vector<vector<int>>& B) {
+        int m = A.size(), n = A[0].size();
+        vector<vector<int>> res(m, vector<int>(B[0].size(),0));
+
+        // for(int i = 0; i < m; i++){
+        //     for(int k = 0; k < n; k++){
+        //          for(int j = 0; j < n; j++){
+        //              res[i][j] += A[i][k] * B[k][j];
+        //          }
+        //     }
+        // }
+        // imporved on upper version, this is a math solution
+        for(int i = 0; i < m; i++){
+            for(int k = 0; k < n; k++){
+                if(A[i][k] != 0)
+                    for(int j = 0; j < B[0].size(); j++){
+                        res[i][j] += A[i][k] * B[k][j];
+                    }
+            }
+        }
+        return res;
+    }
+};
