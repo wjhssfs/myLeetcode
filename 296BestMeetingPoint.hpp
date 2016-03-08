75da533075380a46eef0236e352fa1d15ef2a26a
wjhssfs <wjhssfs@gmail.com>
2016-03-07 22:03:59 -0800

    updates

--- 296BestMeetingPoint.hpp
+++ 296BestMeetingPoint.hpp
@@ -52,3 +52,82 @@ public:
         return rowMin + columnMin;
     }
 };
+
+
+int minTotalDistance(vector<vector<int>>& grid) {
+    const int row = grid.size();
+    if (0 == row) return 0;
+    const int col = grid[0].size();
+    int total = 0;
+    vector<int> posR, posC;
+    for (int i = 0; i < row; ++i) 
+     for (int j = 0; j < col; ++j) {
+         if (grid[i][j] == 1) {
+             posR.emplace_back(i);
+             posC.emplace_back(j);
+         }
+     }
+    int med1 = posR[posR.size() / 2];
+    nth_element(posC.begin(), posC.begin() +  posC.size() / 2, posC.end());
+    int med2 = posC[posC.size() / 2];
+    for (auto pos1 : posR) total += abs(pos1 - med1);
+    for (auto pos2 : posC) total += abs(pos2 - med2);
+    return total;
+}
+
+public int minTotalDistance(int[][] grid) {
+    int m = grid.length;
+    int n = grid[0].length;
+
+    List<Integer> I = new ArrayList<>(m);
+    List<Integer> J = new ArrayList<>(n);
+
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            if(grid[i][j] == 1){
+                I.add(i);
+                J.add(j);
+            }
+        }
+    }
+
+    return getMin(I) + getMin(J);
+}
+
+private int getMin(List<Integer> list){
+    int ret = 0;
+
+    Collections.sort(list);
+
+    int i = 0;
+    int j = list.size() - 1;
+    while(i < j){
+        ret += list.get(j--) - list.get(i++);
+    }
+
+    return ret;
+}
+
+
+//https://leetcode.com/discuss/65366/o-mn-java-2ms
+public int minTotalDistance(int[][] grid) {
+    int m = grid.length, n = grid[0].length;
+    int[] I = new int[m], J = new int[n];
+    for (int i=0; i<m; ++i)
+        for (int j=0; j<n; ++j)
+            if (grid[i][j] == 1) {
+                ++I[i];
+                ++J[j];
+            }
+    int total = 0;
+    for (int[] K : new int[][]{ I, J }) {
+        int i = 0, j = K.length - 1;
+        while (i < j) {
+            int k = Math.min(K[i], K[j]);
+            total += k * (j - i);
+            if ((K[i] -= k) == 0) ++i;
+            if ((K[j] -= k) == 0) --j;
+        }
+    }
+    return total;
+}
