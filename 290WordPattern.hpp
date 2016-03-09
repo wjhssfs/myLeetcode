49ccd9ae9d6bad688e7694c9fc2b272176304a6d
wjhssfs <wjhssfs@gmail.com>
2016-03-09 11:23:04 -0800

    update

--- 290WordPattern.hpp
+++ 290WordPattern.hpp
@@ -36,3 +36,41 @@ public:
         return true;
     }
 };
+
+class Solution(object):
+    def wordPattern(self, pattern, str):
+        if len(pattern) != len(slist):
+            return False
+        return (len(set(pattern)) == len(set(slist)) == len(set(zip(pattern, slist))))
+
+bool wordPattern(string pattern, string str) {
+    map<char, int> p2i;
+    map<string, int> w2i;
+    istringstream in(str);
+    int i = 0, n = pattern.size();
+    for (string word; in >> word; ++i) {
+        if (i == n || p2i[pattern[i]] != w2i[word])
+            return false;
+        p2i[pattern[i]] = w2i[word] = i + 1; // avoid default value 0.
+    }
+    return i == n;
+}
+
+  bool wordPattern(string pattern, string str) {
+        istringstream strcin(str);
+        string s;
+        vector<string> vs;
+        while(strcin >> s) vs.push_back(s);
+        if (pattern.size() != vs.size()) return false;
+        map<string, char> s2c;
+        map<char, string> c2s;
+        for (int i = 0; i < vs.size(); ++i) {
+            if (s2c[vs[i]] == 0 && c2s[pattern[i]] == "") { 
+                s2c[vs[i]] = pattern[i]; 
+                c2s[pattern[i]] = vs[i]; 
+                continue; 
+            }
+            if (s2c[vs[i]] != pattern[i]) return false;
+        }
+        return true;
+    }
