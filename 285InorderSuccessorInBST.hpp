793c517467047d5362337337f8bc7533f392c162
wjhssfs <wjhssfs@gmail.com>
2016-03-13 22:57:41 -0700

    update

--- 285InorderSuccessorInBST.hpp
+++ 285InorderSuccessorInBST.hpp
@@ -35,9 +35,50 @@ public:
     }
 };
 
+// https://leetcode.com/discuss/77805/java-5ms-short-code-with-explanations
 TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
     TreeNode* candidate = NULL;
     while (root)
         root = (root->val > p->val) ? (candidate = root)->left : root->right;
     return candidate;
+}
+
+public TreeNode successor(TreeNode root, TreeNode p) {
+  if (root == null)
+    return null;
+
+  if (root.val <= p.val) {
+    return successor(root.right, p);
+  } else {
+    TreeNode left = successor(root.left, p);
+    return (left != null) ? left : root;
+  }
+}
+
+public TreeNode predecessor(TreeNode root, TreeNode p) {
+  if (root == null)
+    return null;
+
+  if (root.val >= p.val) {
+    return predecessor(root.left, p);
+  } else {
+    TreeNode right = predecessor(root.right, p);
+    return (right != null) ? right : root;
+  }
+}
+
+
+public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+    while (root != null && root.val <= p.val)
+        root = root.right;
+    if (root == null)
+        return null;
+    TreeNode left = inorderSuccessor(root.left, p);
+    return (left != null && left.val > p.val) ? left : root;
+}
+public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+    while (root != null && root.val <= p.val)
+        root = root.right;
+    TreeNode left = root == null ? null : inorderSuccessor(root.left, p);
+    return (left != null && left.val > p.val) ? left : root;
 }
