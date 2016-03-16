cbc54723b1626ae77212890ca01b3f31f3110a26
wjhssfs <wjhssfs@gmail.com>
2016-03-15 23:08:14 -0700

    update

--- 284PeekingIterator.hpp
+++ 284PeekingIterator.hpp
@@ -69,3 +69,27 @@ private:
     int _cur;
     bool _curSet;
 };
+
+
+class PeekingIterator : public Iterator
+{
+public:
+    PeekingIterator(const vector<int> &nums) : Iterator(nums)
+    {
+    }
+
+    int peek()
+    {
+        return Iterator(*this).next();
+    }
+
+    int next()
+    {
+        return Iterator::next();
+    }
+
+    bool hasNext() const
+    {
+        return Iterator::hasNext();
+    }
+};
