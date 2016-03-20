7589392e72dd41405eee373f931a5f61d5d3fce4
wjhssfs <wjhssfs@gmail.com>
2016-03-19 23:15:29 -0700

    updates

--- 275H-IndexII.hpp
+++ 275H-IndexII.hpp
@@ -1,25 +1,18 @@
 // 275 H-Index II
-
 // Follow up for H-Index: What if the citations array is sorted in ascending order? Could you optimize your algorithm?
-
 // Hint:
-
 // Expected runtime complexity is in O(log n) and the input is sorted.
 
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        int n = (int)citations.size();
-        if(!n) return 0;
-        int l = 0, h = n - 1, m = (l+h)/2;
-        while(l < h){
-            if(citations[m] >= (n - m)) {
-                h = m;
-            }
-            else l = m + 1;
-            m = (l+h)/2;
+        int left=0, len = citations.size(), right= len-1,  mid;
+        while(left<=right)
+        {
+            mid=left+ (right-left)/2;
+            if(citations[mid] >= (len-mid)) right = mid - 1;
+            else left = mid + 1;
         }
-        if(citations[h] < n - h) return 0;
-        return n - h;
+        return len - left;
     }
 };
