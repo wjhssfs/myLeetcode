aee3c5c847bc4c0033838350bbc76c39b613fbed
wjhssfs <wjhssfs@gmail.com>
2016-04-04 08:53:45 -0700

    updates

--- 240Searcha2DMatrixII.hpp
+++ 240Searcha2DMatrixII.hpp
@@ -1,13 +1,9 @@
 // 240 Search a 2D Matrix II
-
 // Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
-
 // Integers in each row are sorted in ascending from left to right.
 // Integers in each column are sorted in ascending from top to bottom.
 // For example,
-
 // Consider the following matrix:
-
 // [
 //   [1,   4,  7, 11, 15],
 //   [2,   5,  8, 12, 19],
@@ -16,7 +12,6 @@
 //   [18, 21, 23, 26, 30]
 // ]
 // Given target = 5, return true.
-
 // Given target = 20, return false.
 
 class Solution {
