5c20d379c9a527b425d7742d3cadf143eb321d40
wjhssfs <wjhssfs@gmail.com>
2016-04-04 20:43:12 -0700

    updates

--- 234PalindromeLinkedList.hpp
+++ 234PalindromeLinkedList.hpp
@@ -1,9 +1,7 @@
 // 234 Palindrome Linked List 
 // Given a singly linked list, determine if it is a palindrome.
-
 // Follow up:
 // Could you do it in O(n) time and O(1) space?
-
 /**
  * Definition for singly-linked list.
  * struct ListNode {
