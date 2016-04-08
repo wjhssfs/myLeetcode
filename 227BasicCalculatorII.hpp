0580b5597df57dd31ca2a839e786a4a51ca7e7cb
wjhssfs <wjhssfs@gmail.com>
2016-04-07 21:36:23 -0700

    update

--- 227BasicCalculatorII.hpp
+++ 227BasicCalculatorII.hpp
@@ -1,17 +1,11 @@
 // 227 Basic Calculator II
-
 // Implement a basic calculator to evaluate a simple expression string.
-
 // The expression string contains only non-negative integers, +, -, *, / operators and empty spaces . The integer division should truncate toward zero.
-
 // You may assume that the given expression is always valid.
-
 // Some examples:
 // "3+2*2" = 7
 // " 3/2 " = 1
 // " 3+5 / 2 " = 5
-
-
 class Solution {
 public:
     int calculate(string s) {
