35f1cbb1fa463c217ed2869e19565287a0fc54f5
wjhssfs <wjhssfs@gmail.com>
2016-04-12 09:16:48 -0700

    update

--- 221MaximalSquare.hpp
+++ 221MaximalSquare.hpp
@@ -1,48 +1,40 @@
 // 221 Maximal Square
-
 // Given a 2D binary matrix filled with 0's and 1's, find the largest square containing all 1's and return its area.
-
 // For example, given the following matrix:
-
 // 1 0 1 0 0
 // 1 0 1 1 1
 // 1 1 1 1 1
 // 1 0 0 1 0
 // Return 4.
 
+// https://leetcode.com/discuss/38489/easy-solution-with-detailed-explanations-8ms-time-and-space
+// preDp[i-1]        preDp[i] = dp[ic](cur)
+// dp[ic-1](cur)     dp[ic](next)
+// dp[ic](next) = min(dp[ic], dp[ic-1], preDP[i-1]) + 1
 class Solution {
 public:
-    int maximalSquare(vector<vector<char>>& matrix) {
-        int res = 0, rows = (int)matrix.size(), cols = 0;
-        if(rows) cols = (int)matrix[0].size();
-        if(!rows || !cols) return 0;
-        // # of 1s above and # of 1s to the left
-        vector<vector<pair<int, int>>> m(rows, vector<pair<int,int>>(cols));
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                m[i][j] = matrix[i][j]=='0'? make_pair(0,0) :
-                    make_pair(i==0?1:m[i-1][j].first + 1, j==0?1:m[i][j-1].second + 1);
-                int curMaxLength = maxSquareLen(m, i, j);
-                res = max(res, curMaxLength*curMaxLength);
-            }
-        }
-        return res;
-    }
-    
-    int maxSquareLen(vector<vector<pair<int, int>>> &m, int x, int y){
-        int layerBound = min(m[x][y].first, m[x][y].second);
-        if(!layerBound) return 0;
-        int layer = 1;
-        while(x - layer >= 0 && y - layer >= 0 && layer < layerBound){
-            int newLayerbound = min(m[x-layer][y-layer].first, m[x-layer][y-layer].second);
-            if(!newLayerbound) break;
-            layerBound = min (newLayerbound + layer, layerBound);
-            layer++;
-        }
-        return layer;
-    }
-};	
+  int maximalSquare(vector<vector<char>>& matrix) {
+    int nr = matrix.size(); if (nr == 0) return 0;
+    int nc = matrix[0].size(); if (nc == 0) return 0;
 
+    vector<int> dp(nc+1, 0);
+    int maxsize = 0;
+    int last_topleft = 0;  // This is 'preDp[i-1]' for the current element
+
+    for (int ir = 1; ir <= nr; ++ir) {
+        for (int ic = 1; ic <= nc; ++ic) {
+            int temp = dp[ic];      // This is 'preDp[i-1]' for the next element                
+             if (matrix[ir-1][ic-1] == '0') dp[ic] = 0; 
+            else {
+                dp[ic] = min(min(dp[ic], dp[ic-1]), last_topleft) + 1;
+                maxsize = max(maxsize, dp[ic]);
+            }
+            last_topleft = temp;  // update 'preDp[i-1]'
+        }
+    }
+    return maxsize * maxsize;
+  }
+};
 
 public int maximalSquare(char[][] a) {
   if (a == null || a.length == 0 || a[0].length == 0)
