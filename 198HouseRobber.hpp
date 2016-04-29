5336d84e7d471803f2268f3ea8cf6058ba853899
wjhssfs <wjhssfs@gmail.com>
2016-04-28 22:12:13 -0700

    updates

--- 198HouseRobber.hpp
+++ 198HouseRobber.hpp
@@ -1,9 +1,7 @@
 // 198 House Robber 
-// 
-// You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
-// 
+// You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of
+// them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
 // Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
-
 class Solution {
 public:
     int rob(vector<int>& nums) {
@@ -17,3 +15,14 @@ public:
         return max(noRob.back(), rob.back());
     }
 };
+
+public int rob(int[] num) {
+    int prevNo = 0;
+    int prevYes = 0;
+    for (int n : num) {
+        int temp = prevNo;
+        prevNo = Math.max(prevNo, prevYes);
+        prevYes = n + temp;
+    }
+    return Math.max(prevNo, prevYes);
+}
