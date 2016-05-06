9f209859884901d9d5d1918a209d61fdba4315bc
wjhssfs <wjhssfs@gmail.com>
2016-05-05 23:08:58 -0700

    updates

--- 168ExcelSheetColumnTitle.hpp
+++ 168ExcelSheetColumnTitle.hpp
@@ -1,8 +1,6 @@
 // 168 Excel Sheet Column Title 
 // Given a positive integer, return its corresponding column title as appear in an Excel sheet.
-// 
 // For example:
-// 
 //     1 -> A
 //     2 -> B
 //     3 -> C
@@ -12,7 +10,6 @@
 //     28 -> AB 
 
 // http://articles.leetcode.com/2010/10/amazon-bar-raiser-interview-question.html
-
 class Solution {
 public:
     string convertToTitle(int n) {
