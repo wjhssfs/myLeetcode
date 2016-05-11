3f6ee2707566e7d6c0f1b697fb843f5faafc0167
wjhssfs <wjhssfs@gmail.com>
2016-05-11 09:08:16 -0700

    updates

--- 135Candy.hpp
+++ 135Candy.hpp
@@ -1,8 +1,6 @@
 // 135 Candy 
 // There are N children standing in a line. Each child is assigned a rating value.
-// 
 // You are giving candies to these children subjected to the following requirements:
-// 
 // Each child must have at least one candy.
 // Children with a higher rating get more candies than their neighbors.
 // What is the minimum candies you must give?
@@ -61,3 +59,27 @@ public:
         return res;
     }
 };
+
+public class Solution {
+    public int candy(int[] ratings) {
+        if (ratings == null || ratings.length == 0) return 0;
+        int total = 1, prev = 1, countDown = 0;
+        for (int i = 1; i < ratings.length; i++) {
+            if (ratings[i] >= ratings[i-1]) {
+                if (countDown > 0) {
+                    total += countDown*(countDown+1)/2; // arithmetic progression
+                    if (countDown >= prev) total += countDown - prev + 1;
+                    countDown = 0;
+                    prev = 1;
+                }
+                prev = ratings[i] == ratings[i-1] ? 1 : prev+1;
+                total += prev;
+            } else countDown++;
+        }
+        if (countDown > 0) { // if we were descending at the end
+            total += countDown*(countDown+1)/2;
+            if (countDown >= prev) total += countDown - prev + 1;
+        }
+        return total;
+    }
+}
--- 137SingleNumberII.hpp
+++ 137SingleNumberII.hpp
@@ -1,6 +1,5 @@
 // 138 Single Number II
 // Given an array of integers, every element appears three times except for one. Find that single one.
-// 
 // Note:
 // Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
 
@@ -20,3 +19,14 @@ public:
         return res;
     }
 };
+
+//https://leetcode.com/discuss/9763/accepted-proper-explaination-does-anyone-have-better-idea
+// https://leetcode.com/discuss/6632/challenge-me-thx
+public int singleNumber(int[] A) {
+    int ones = 0, twos = 0;
+    for(int i = 0; i < A.length; i++){
+        ones = (ones ^ A[i]) & ~twos;
+        twos = (twos ^ A[i]) & ~ones;
+    }
+    return ones;
+}
