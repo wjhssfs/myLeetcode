812a56518612370432b7599766fefe2dda1478b0
wjhssfs <wjhssfs@gmail.com>
2016-05-11 23:51:24 -0700

    updates

--- 132PalindromePartitioningII.hpp
+++ 132PalindromePartitioningII.hpp
@@ -1,9 +1,6 @@
 // 132 Palindrome Partitioning II
-// 
 // Given a string s, partition s such that every substring of the partition is a palindrome.
-// 
 // Return the minimum cuts needed for a palindrome partitioning of s.
-// 
 // For example, given s = "aab",
 // Return 1 since the palindrome partitioning ["aa","b"] could be produced using 1 cut.
 
@@ -27,5 +24,46 @@ public:
             }
         }
         return dp[N-1];
+    }
+};
+
+
+// cut[i] is the minimum of cut[j - 1] + 1 (j <= i), if [j, i] is palindrome.
+// If [j, i] is palindrome, [j + 1, i - 1] is palindrome, and c[j] == c[i].
+
+public int minCut(String s) {
+    char[] c = s.toCharArray();
+    int n = c.length;
+    int[] cut = new int[n];
+    boolean[][] pal = new boolean[n][n];
+
+    for(int i = 0; i < n; i++) {
+        int min = i;
+        for(int j = 0; j <= i; j++) {
+            if(c[j] == c[i] && (j + 1 > i - 1 || pal[j + 1][i - 1])) {
+                pal[j][i] = true;  
+                min = j == 0 ? 0 : Math.min(min, cut[j - 1] + 1);
+            }
+        }
+        cut[i] = min;
+    }
+    return cut[n - 1];
+}
+
+// https://leetcode.com/discuss/47140/two-versions-given-one-28ms-one-manancher-like-algorithm-10
+class Solution {
+public:
+    int minCut(string s) {
+        int n = s.size();
+        vector<int> cut(n+1, 0);  // number of cuts for the first k characters
+        for (int i = 0; i <= n; i++) cut[i] = i-1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; i-j >= 0 && i+j < n && s[i-j]==s[i+j] ; j++) // odd length palindrome
+                cut[i+j+1] = min(cut[i+j+1],1+cut[i-j]);
+
+            for (int j = 1; i-j+1 >= 0 && i+j < n && s[i-j+1] == s[i+j]; j++) // even length palindrome
+                cut[i+j+1] = min(cut[i+j+1],1+cut[i-j+1]);
+        }
+        return cut[n];
     }
 };
--- 134GasStation.hpp
+++ 134GasStation.hpp
@@ -1,13 +1,11 @@
 // 134 Gas Station 
 // There are N gas stations along a circular route, where the amount of gas at station i is gas[i].
-// 
-// You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from station i to its next station (i+1). You begin the journey with an empty tank at one of the gas stations.
-// 
+// You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from station i to its
+// next station (i+1). You begin the journey with an empty tank at one of the gas stations.
 // Return the starting gas station's index if you can travel around the circuit once, otherwise return -1.
 // 
 // Note:
 // The solution is guaranteed to be unique.
-
 class Solution {
 public:
 	int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
@@ -26,4 +24,32 @@ public:
 		}
 		return -1;
 	}
+};
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
+        int i, j, n = gas.size();
+
+        /*
+         * If start from i, stop before station x -> no station k from i + 1 to x - 1 can reach x.
+         * Bcoz if so, i can reach k and k can reach x, then i reaches x. Contradiction.
+         * Thus i can jump directly to x instead of i + 1, bringing complexity from O(n^2) to O(n).
+         */
+        // start from station i
+        for (i = 0; i < n; i += j) {
+            int gas_left = 0;
+            // forward j stations
+            for (j = 1; j <= n; j++) {
+                int k = (i + j - 1) % n;
+                gas_left += gas[k] - cost[k];
+                if (gas_left < 0)
+                    break;
+            }
+            if (j > n)
+                return i;
+        }
+
+        return -1;
+    }
 };
