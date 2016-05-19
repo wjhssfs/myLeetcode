6a03d6f0e1c5c1b7b76d177c0fd173950c084ff6
wjhssfs <wjhssfs@gmail.com>
2016-05-19 09:38:13 -0700

    updates

--- 090SubsetsII.hpp
+++ 090SubsetsII.hpp
@@ -1,12 +1,10 @@
 // 90 Subsets II 
-// Given a collection of integers that might contain duplicates, S, return all possible subsets.
-// 
+// Given a collection of integers that might contain duplicates, mums, return all possible subsets.
 // Note:
 // Elements in a subset must be in non-descending order.
 // The solution set must not contain duplicate subsets.
 // For example,
 // If S = [1,2,2], a solution is:
-// 
 // [
 //   [2],
 //   [1],
@@ -18,28 +16,44 @@
 
 class Solution {
 public:
-    vector<vector<int>> subsetsWithDup(vector<int> &S) {
-        vector<vector<int> > res(1, vector<int>());
-        sort(S.begin(), S.end());
-        vector<int> set;
-        int N = S.size();
-        for (int l = 1; l <= N; ++l)
-            subsetsWithDupRe(S, l, 0, set, res);
+    std::vector<std::vector<int> > subsetsWithDup(std::vector<int> &nums) {
+        std::sort(nums.begin(), nums.end());
+        std::vector<std::vector<int> > res;
+        std::vector<int> vec;
+        subsetsWithDup(res, nums, vec, 0);
         return res;
     }
-
-    void subsetsWithDupRe(vector<int> &S, int L, int start, vector<int> &set, vector<vector<int>> &res)
-    {
-        int N = S.size(), M = set.size();
-        if (M == L) {
-            res.push_back(set);
-            return;
-        }
-        for (int i = start; i <= N - (L - M); ++i) {
-            if (i > start && S[i] == S[i-1]) continue;
-            set.push_back(S[i]);
-            subsetsWithDupRe(S, L, i + 1, set, res);
-            set.pop_back();
+private:
+    void subsetsWithDup(std::vector<std::vector<int> > &res, std::vector<int> &nums, std::vector<int> &vec, int begin) {
+        res.push_back(vec);
+        for (int i = begin; i != nums.size(); ++i) {
+            if (i == begin || nums[i] != nums[i - 1]) { 
+                vec.push_back(nums[i]);
+                subsetsWithDup(res, nums, vec, i + 1);
+                vec.pop_back();
+            }
         }
     }
+};
+
+class Solution {
+public:
+    vector<vector<int> > subsetsWithDup(vector<int> &S) {
+        vector<vector<int> > totalset = {{}};
+        sort(S.begin(),S.end());
+        for(int i=0; i<S.size();){
+            int count = 0; // num of elements are the same
+            while(count + i<S.size() && S[count+i]==S[i])  count++;
+            int previousN = totalset.size();
+            for(int k=0; k<previousN; k++){
+                vector<int> instance = totalset[k];
+                for(int j=0; j<count; j++){
+                    instance.push_back(S[i]);
+                    totalset.push_back(instance);
+                }
+            }
+            i += count;
+        }
+        return totalset;
+        }
 };
