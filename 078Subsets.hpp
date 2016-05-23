2b55d89dcc57cb59ea7db2aedddab1470b91c9ba
wjhssfs <wjhssfs@gmail.com>
2016-05-22 23:19:36 -0700

    updates

--- 078Subsets.hpp
+++ 078Subsets.hpp
@@ -1,6 +1,5 @@
 // 78 Subsets 
 // Given a set of distinct integers, S, return all possible subsets.
-// 
 // Note:
 // Elements in a subset must be in non-descending order.
 // The solution set must not contain duplicate subsets.
@@ -20,25 +19,56 @@
 
 class Solution {
 public:
-    vector<vector<int> > subsets(vector<int> &S) {
-        sort(S.begin(), S.end());
-        result.clear(); candidate.clear();
-        recursiveGen(S, 0);
-        return result;
+    vector<vector<int>> subsets(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> subs;
+        vector<int> sub;  
+        genSubsets(nums, 0, sub, subs);
+        return subs; 
     }
-private:
-    void recursiveGen(vector<int> &S, int index)
-    {
-        if (index == S.size()) {
-            result.push_back(candidate);
-            return;
+    void genSubsets(vector<int>& nums, int start, vector<int>& sub, vector<vector<int>>& subs) {
+        subs.push_back(sub);
+        for (int i = start; i < nums.size(); i++) {
+            sub.push_back(nums[i]);
+            genSubsets(nums, i + 1, sub, subs);
+            sub.pop_back();
         }
-        candidate.push_back(S[index]);
-        recursiveGen(S, index+1);
-        candidate.pop_back();
-        recursiveGen(S, index + 1);
-
     }
-    vector<vector<int> > result;
-    vector<int> candidate;
 };
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> subs(1, vector<int>());
+        for (int i = 0; i < nums.size(); i++) {
+            int n = subs.size();
+            for (int j = 0; j < n; j++) {
+                subs.push_back(subs[j]); 
+                subs.back().push_back(nums[i]);
+            }
+        }
+        return subs;
+    }
+}; 
+
+// https://leetcode.com/discuss/46668/recursive-iterative-manipulation-solutions-explanations
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int num_subset = pow(2, nums.size()); 
+        vector<vector<int> > res(num_subset, vector<int>());
+        for (int i = 0; i < nums.size(); i++)
+            for (int j = 0; j < num_subset; j++)
+                if ((j >> i) & 1)
+                    res[j].push_back(nums[i]);
+        return res;  
+    }
+};
+
+"only iterate over the 1-bits"
+    for (int i = 0; i < nums.size(); i++)
+        for (int step = 1 << i, start = 0; start < num_subset; start += 2 * step)
+            for (int j = start; j < start + step; j++)
+                res[j].push_back(nums[i]);
--- 079WordSearch.hpp
+++ 079WordSearch.hpp
@@ -1,11 +1,9 @@
 // 79 Word Search
 // Given a 2D board and a word, find if the word exists in the grid.
-// 
-// The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
-// 
+// The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those
+// horizontally or vertically neighboring. The same letter cell may not be used more than once. 
 // For example,
 // Given board =
-// 
 // [
 //   ["ABCE"],
 //   ["SFCS"],
@@ -46,3 +44,26 @@ public:
         return false;
     }
 };
+
+public boolean exist(char[][] board, String word) {
+    for (int i = 0; i < board.length; i++) {
+        for (int j = 0; j < board[i].length; j++) {
+            if(exist(board, i, j, word, 0)) return true;
+        }
+    }
+    return false;
+}
+
+private boolean exist(char[][] board, int x, int y, String word, int start) {
+    if(start >= word.length()) return true;
+    if(x < 0 || x >= board.length || y < 0 || y >= board[0].length) return false;
+    if (board[x][y] == word.charAt(start++)) {
+        char c = board[x][y];
+        board[x][y] = '#';
+        boolean res = exist(board, x + 1, y, word, start) || exist(board, x - 1, y, word, start) ||
+        exist(board, x, y + 1, word, start) || exist(board, x, y - 1, word, start);
+        board[x][y] = c;
+        return res;
+    }
+    return false;
+}
