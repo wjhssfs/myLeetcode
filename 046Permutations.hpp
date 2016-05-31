a27505b89739d4e831ed58119f564fb62258c92e
wjhssfs <wjhssfs@gmail.com>
2016-05-31 09:45:59 -0700

    updates

--- 046Permutations.hpp
+++ 046Permutations.hpp
@@ -1,7 +1,5 @@
 // 46 Permutations 
-
 // Given a collection of numbers, return all possible permutations.
-
 // For example,
 // [1,2,3] have the following permutations:
 // [1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1].
@@ -73,3 +71,50 @@ public:
         }
     }
 };
+
+class Solution {
+public:
+    vector<vector<int> > permute(vector<int> &num) {
+        vector<vector<int> > result;
+
+        permuteRecursive(num, 0, result);
+        return result;
+    }
+
+    // permute num[begin..end]
+    // invariant: num[0..begin-1] have been fixed/permuted
+    void permuteRecursive(vector<int> &num, int begin, vector<vector<int> > &result)    {
+        if (begin >= num.size() - 1) {
+            // one permutation instance
+            result.push_back(num);
+            return;
+        }
+
+        for (int i = begin; i < num.size(); i++) {
+            swap(num[begin], num[i]);
+            permuteRecursive(num, begin + 1, result);
+            // reset
+            swap(num[begin], num[i]);
+        }
+    }
+};
+
+public List<List<Integer>> permute(int[] num) {
+    List<List<Integer>> ans = new ArrayList<List<Integer>>();
+    if (num.length ==0) return ans;
+    List<Integer> l0 = new ArrayList<Integer>();
+    l0.add(num[0]);
+    ans.add(l0);
+    for (int i = 1; i< num.length; ++i){
+        List<List<Integer>> new_ans = new ArrayList<List<Integer>>(); 
+        for (int j = 0; j<=i; ++j){            
+           for (List<Integer> l : ans){
+               List<Integer> new_l = new ArrayList<Integer>(l);
+               new_l.add(j,num[i]);
+               new_ans.add(new_l);
+           }
+        }
+        ans = new_ans;
+    }
+    return ans;
+}
--- 047PermutationsII.hpp
+++ 047PermutationsII.hpp
@@ -1,6 +1,5 @@
 // 47 Permutations II 
 // Given a collection of numbers that might contain duplicates, return all possible unique permutations.
-
 // For example,
 // [1,1,2] have the following unique permutations:
 // [1,1,2], [1,2,1], and [2,1,1].
@@ -77,3 +76,24 @@ public:
     }
 };
 
+class Solution {
+public:
+    void recursion(vector<int> &num, int i, int j, vector<vector<int> > &res) {
+        if (i == j-1) {
+            res.push_back(num);
+            return;
+        }
+        for (int k = i; k < j; k++) {
+            if (i != k && num[i] == num[k]) continue;
+            swap(num[i], num[k]);
+            recursion(num, i+1, j, res);
+            swap(num[i], num[k]);
+        }
+    }
+    vector<vector<int> > permuteUnique(vector<int> &num) {
+        sort(num.begin(), num.end());
+        vector<vector<int> >res;
+        recursion(num, 0, num.size(), res);
+        return res;
+    }
+};
