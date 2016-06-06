5ced9eeb17c9b938bb54f6557e1c280d4b854cc7
wjhssfs <wjhssfs@gmail.com>
2016-06-05 22:36:39 -0700

    updates

--- 034SearchforaRange.hpp
+++ 034SearchforaRange.hpp
@@ -1,10 +1,7 @@
 // 34 Search for a Range
 // Given a sorted array of integers, find the starting and ending position of a given target value.
-
 // Your algorithm's runtime complexity must be in the order of O(log n).
-
 // If the target is not found in the array, return [-1, -1].
-
 // For example,
 // Given [5, 7, 7, 8, 8, 10] and target value 8,
 // return [3, 4].
@@ -18,7 +15,7 @@ public:
             if(A[mid] < target) low = mid + 1;
             else high = mid - 1;
         }
-        if(A[low] != target) return {-1, -1};
+        if(low == n || A[low] != target) return {-1, -1};
         int lowBound = low;
         low = 0, high = n-1;
         while(low <= high){
@@ -29,3 +26,59 @@ public:
         return {lowBound, high};
     }
 };
+
+
+// https://leetcode.com/discuss/18242/clean-iterative-solution-binary-searches-with-explanation
+vector<int> searchRange(int A[], int n, int target) {
+    int i = 0, j = n - 1;
+    vector<int> ret(2, -1);
+    // Search for the left one
+    while (i < j)
+    {
+        int mid = (i + j) /2;
+        if (A[mid] < target) i = mid + 1;
+        else j = mid;
+    }
+    if (A[i]!=target) return ret;
+    else ret[0] = i;
+
+    // Search for the right one
+    j = n-1;  // We don't have to set i to 0 the second time.
+    while (i < j)
+    {
+        int mid = (i + j) /2 + 1;   // Make mid biased to the right
+        if (A[mid] > target) j = mid - 1;  
+        else i = mid;               // So that this won't make the search range stuck.
+    }
+    ret[1] = j;
+    return ret; 
+}
+
+public class Solution {
+    public int[] searchRange(int[] A, int target) {
+        int start = Solution.firstGreaterEqual(A, target);
+        if (start == A.length || A[start] != target) {
+            return new int[]{-1, -1};
+        }
+        return new int[]{start, Solution.firstGreaterEqual(A, target + 1) - 1};
+    }
+
+    //find the first number that is greater than or equal to target.
+    //could return A.length if target is greater than A[A.length-1].
+    //actually this is the same as lower_bound in C++ STL.
+    private static int firstGreaterEqual(int[] A, int target) {
+        int low = 0, high = A.length;
+        while (low < high) {
+            int mid = low + ((high - low) >> 1);
+            //low <= mid < high
+            if (A[mid] < target) {
+                low = mid + 1;
+            } else {
+                //should not be mid-1 when A[mid]==target.
+                //could be mid even if A[mid]>target because mid<high.
+                high = mid;
+            }
+        }
+        return low;
+    }
+}
--- 036ValidSudoku.hpp
+++ 036ValidSudoku.hpp
@@ -2,9 +2,7 @@
 // Determine if a Sudoku is valid, according to: Sudoku Puzzles - The Rules.
 // http://sudoku.com.au/TheRules.aspx
 // The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
-
 // A partially filled sudoku which is valid.
-
 // Note:
 // A valid Sudoku board (partially filled) is not necessarily solvable. Only the filled cells need to be validated.
 
@@ -33,6 +31,27 @@ public:
                 }
             }
         }
+        return true;
+    }
+};
+
+class Solution
+{
+public:
+    bool isValidSudoku(vector<vector<char> > &board)
+    {
+        bool rowUsed[9][9] = {0}, colUsed[9][9] = {0}, squareUsed[9][9] = {0};
+
+        for(int i = 0; i < board.size(); ++ i)
+            for(int j = 0; j < board[i].size(); ++ j)
+                if(board[i][j] != '.')
+                {
+                    int num = board[i][j] - '0' - 1, k = i / 3 * 3 + j / 3;
+                    if(rowUsed[i][num] || colUsed[j][num] || squareUsed[k][num])
+                        return false;
+                    rowUsed[i][num] = colUsed[j][num] = squareUsed[k][num] = true;
+                }
+
         return true;
     }
 };
