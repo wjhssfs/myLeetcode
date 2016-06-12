d88b7668cb6561bada4ca94bb2f980633af9be01
wjhssfs <wjhssfs@gmail.com>
2016-06-12 16:02:07 -0700

    updates

--- 021MergeTwoSortedLists.hpp
+++ 021MergeTwoSortedLists.hpp
@@ -1,5 +1,6 @@
 // 21 Merge Two Sorted Lists 
-// Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+// Merge two sorted linked lists and return it as a new list. The new list should
+// be made by splicing together the nodes of the first two lists.
 
 /**
  * Definition for singly-linked list.
@@ -29,20 +30,36 @@ public:
     }
 };
 
-
-class Solution2 {
+class Solution {
 public:
     ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
-        ListNode head(0), *cur = &head;
-        while (l1 && l2) 
-        {
-            ListNode **min = l1->val < l2->val ? &l1 : &l2;
-            cur->next = *min;
-            cur = cur->next;
-            *min = (*min)->next;
+        ListNode dummy(INT_MIN);
+        ListNode *tail = &dummy;
+
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                tail->next = l1;
+                l1 = l1->next;
+            } else {
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            tail = tail->next;
         }
-        if (l1) cur->next = l1;
-        if (l2) cur->next = l2;
-        return head.next;
+
+        tail->next = l1 ? l1 : l2;
+        return dummy.next;
     }
 };
+
+public ListNode mergeTwoLists(ListNode l1, ListNode l2){
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        if(l1.val < l2.val){
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else{
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+}
--- 022GenerateParentheses.hpp
+++ 022GenerateParentheses.hpp
@@ -1,28 +1,51 @@
 // 22 Generate Parentheses 
 // Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
-
 // For example, given n = 3, a solution set is:
-
 // "((()))", "(()())", "(())()", "()(())", "()()()"
 
 class Solution {
 public:
-	vector<string> generateParenthesis(int n) {
-		vector<string> results;
-		generateParenthesisInner(n, 0, 0, "", results);
-		return results;
-	}
-
-	void generateParenthesisInner(int n, int opened, int closed, string current, vector<string> &results){
-		if (n == opened && n == closed){
-			results.push_back(current);
-			return;
-		}
-		if (closed < opened){
-			generateParenthesisInner(n, opened, closed + 1, current + ")", results);
-		}
-		if (opened < n){
-			generateParenthesisInner(n, opened + 1, closed, current + "(", results);
-		}
-	}
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        addingpar(res, "", n, 0);
+        return res;
+    }
+    void addingpar(vector<string> &v, string str, int n, int m){
+        if(n==0 && m==0) {
+            v.push_back(str);
+            return;
+        }
+        if(m > 0){ addingpar(v, str+")", n, m-1); }
+        if(n > 0){ addingpar(v, str+"(", n-1, m+1); }
+    }
 };
+
+// https://leetcode.com/discuss/11509/an-iterative-method
+public class Solution
+{
+    public List<String> generateParenthesis(int n)
+    {
+        List<List<String>> lists = new ArrayList<>();
+        lists.add(Collections.singletonList(""));
+
+        for (int i = 1; i <= n; ++i)
+        {
+            final List<String> list = new ArrayList<>();
+
+            for (int j = 0; j < i; ++j)
+            {
+                for (final String first : lists.get(j))
+                {
+                    for (final String second : lists.get(i - 1 - j))
+                    {
+                        list.add("(" + first + ")" + second);
+                    }
+                }
+            }
+
+            lists.add(list);
+        }
+
+        return lists.get(lists.size() - 1);
+    }
+}
