83f9a102d5e1f7a2b7ec2742f59864e42ef921c4
wjhssfs <wjhssfs@gmail.com>
2016-06-15 09:18:09 -0700

    update

--- 010RegularExpressionMatching.hpp
+++ 010RegularExpressionMatching.hpp
@@ -1,12 +1,8 @@
 // 10 Regular Expression Matching 
-
 // Implement regular expression matching with support for '.' and '*'.
-
 // '.' Matches any single character.
 // '*' Matches zero or more of the preceding element.
-
 // The matching should cover the entire input string (not partial).
-
 // The function prototype should be:
 // bool isMatch(const char *s, const char *p)
 
@@ -32,5 +28,70 @@ public:
 	    	return isMatch(s+1, p+1);
 	    }
 	    return false;
+    }
+};
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        if (p.empty())    return s.empty();
+
+        if ('*' == p[1])
+            // x* matches empty string or at least one character: x* -> xx*
+            // *s is to ensure s is non-empty
+            return (isMatch(s, p.substr(2)) || !s.empty() && (s[0] == p[0] || '.' == p[0]) && isMatch(s.substr(1), p));
+        else
+            return !s.empty() && (s[0] == p[0] || '.' == p[0]) && isMatch(s.substr(1), p.substr(1));
+    }
+};
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        /**
+         * f[i][j]: if s[0..i-1] matches p[0..j-1]
+         * if p[j - 1] != '*'
+         *      f[i][j] = f[i - 1][j - 1] && s[i - 1] == p[j - 1]
+         * if p[j - 1] == '*', denote p[j - 2] with x
+         *      f[i][j] is true iff any of the following is true
+         *      1) "x*" repeats 0 time and matches empty: f[i][j - 2]
+         *      2) "x*" repeats >= 1 times and matches "x*x": s[i - 1] == x && f[i - 1][j]
+         * '.' matches any single character
+         */
+        int m = s.size(), n = p.size();
+        vector<vector<bool>> f(m + 1, vector<bool>(n + 1, false));
+
+        f[0][0] = true;
+        for (int i = 1; i <= m; i++)
+            f[i][0] = false;
+        // p[0.., j - 3, j - 2, j - 1] matches empty iff p[j - 1] is '*' and p[0..j - 3] matches empty
+        for (int j = 1; j <= n; j++)
+            f[0][j] = j > 1 && '*' == p[j - 1] && f[0][j - 2];
+
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++)
+                if (p[j - 1] != '*')
+                    f[i][j] = f[i - 1][j - 1] && (s[i - 1] == p[j - 1] || '.' == p[j - 1]);
+                else
+                    // p[0] cannot be '*' so no need to check "j > 1" here
+                    f[i][j] = f[i][j - 2] || (s[i - 1] == p[j - 2] || '.' == p[j - 2]) && f[i - 1][j];
+
+        return f[m][n];
+    }
+};
+
+// https://leetcode.com/discuss/43860/9-lines-16ms-c-dp-solutions-with-explanations
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m = s.length(), n = p.length(); 
+        vector<vector<bool> > dp(m + 1, vector<bool> (n + 1, false));
+        dp[0][0] = true;
+        for (int i = 0; i <= m; i++)
+            for (int j = 1; j <= n; j++)
+                if (p[j - 1] == '*')
+                    dp[i][j] = dp[i][j - 2] || (i > 0 && (s[i - 1] == p[j - 2] || p[j - 2] == '.') && dp[i - 1][j]);
+                else dp[i][j] = i > 0 && dp[i - 1][j - 1] && (s[i - 1] == p[j - 1] || p[j - 1] == '.');
+        return dp[m][n];
     }
 };
