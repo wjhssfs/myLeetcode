7fbf4b7eb72cca47db241e5a82c0b950176f19cf
wjhssfs <wjhssfs@gmail.com>
2016-06-22 01:34:26 -0700

    updates

--- 003LongestSubstringWithoutRepeatingCharacters.hpp
+++ 003LongestSubstringWithoutRepeatingCharacters.hpp
@@ -1,5 +1,7 @@
 // 3 Longest Substring Without Repeating Characters 
-// Given a string, find the length of the longest substring without repeating characters. For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
+// Given a string, find the length of the longest substring without repeating characters.
+// For example, the longest substring without repeating letters for "abcabcbb" is "abc",
+// which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
 
 class Solution {
 public:
@@ -17,3 +19,32 @@ public:
         return maxLen;
     }
 };
+
+
+int lengthOfLongestSubstring(string s) {
+    // for ASCII char sequence, use this as a hashmap
+    vector<int> charIndex(256, -1);
+    int longest = 0, m = 0;
+
+    for (int i = 0; i < s.length(); i++) {
+        m = max(charIndex[s[i]] + 1, m);    // automatically takes care of -1 case
+        charIndex[s[i]] = i;
+        longest = max(longest, i - m + 1);
+    }
+
+    return longest;
+}
+
+  public int lengthOfLongestSubstring(String s) {
+        if (s.length()==0) return 0;
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        int max=0;
+        for (int i=0, j=0; i<s.length(); ++i){
+            if (map.containsKey(s.charAt(i))){
+                j = Math.max(j,map.get(s.charAt(i))+1);
+            }
+            map.put(s.charAt(i),i);
+            max = Math.max(max,i-j+1);
+        }
+        return max;
+    }
