324cc0943b055806f05d9cb973c2f18d7e9e0e15
wjhssfs <wjhssfs@gmail.com>
2016-07-09 19:59:13 -0700

    updates

--- 347TopKFrequentElements.hpp
+++ 347TopKFrequentElements.hpp
@@ -31,3 +31,60 @@ public:
         return ret;
     }
 };
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> map;
+        for(int num : nums){
+            map[num]++;
+        }
+        
+        vector<int> res;
+        // pair<first, second>: first is frequency,  second is number
+        priority_queue<pair<int,int>> pq; 
+        for(auto it = map.begin(); it != map.end(); it++){
+            pq.push(make_pair(it->second, it->first));
+            if(pq.size() > (int)map.size() - k){
+                res.push_back(pq.top().second);
+                pq.pop();
+            }
+        }
+        return res;
+    }
+};
+
+// O(n)
+public List<Integer> topKFrequent(int[] nums, int k) {
+
+    List<Integer>[] bucket = new List[nums.length + 1];
+    Map<Integer, Integer> frequencyMap = new HashMap<Integer, Integer>();
+
+    for (int n : nums) {
+        frequencyMap.put(n, frequencyMap.getOrDefault(n, 0) + 1);
+    }
+
+    for (int key : frequencyMap.keySet()) {
+        int frequency = frequencyMap.get(key);
+        if (bucket[frequency] == null) {
+            bucket[frequency] = new ArrayList<>();
+        }
+        bucket[frequency].add(key);
+    }
+
+    List<Integer> res = new ArrayList<>();
+
+    for (int pos = bucket.length - 1; pos >= 0 && res.size() < k; pos--) {
+        if (bucket[pos] != null) {
+            res.addAll(bucket[pos]);
+        }
+    }
+    return res;
+}
+
+class Solution(object):        
+    def topKFrequent(self, nums, k):
+        bucket = [[] for _ in nums]
+        for num, freq in collections.Counter(nums).items():
+            bucket[len(nums) - freq].append(num)
+        return list(itertools.chain(*bucket))[:k]
---
+++ 349IntersectionOfTwoArrays.hpp
@@ -0,0 +1,43 @@
+// 349. Intersection of Two Arrays
+// Given two arrays, write a function to compute their intersection.
+// Example:
+// Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2].
+// Note:
+// Each element in the result must be unique.
+// The result can be in any order.
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ret;
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        int i = 0, j = 0;
+        while (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] < nums2[j]) {
+                ++i;
+            } else if (nums1[i] == nums2[j]) {
+                ret.push_back(nums1[i]);
+                while (i < nums1.size() && nums1[i] == ret.back()) ++i;
+                while (j < nums2.size() && nums2[j] == ret.back()) ++j;
+            } else {
+                ++j;
+            }
+        }
+        return ret;
+    }
+};
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ret;
+        unordered_set<int> s1(nums1.begin(), nums1.end()), s2(nums2.begin(), nums2.end());
+        for (auto num : s1) {
+            if (s2.count(num)) {
+                ret.push_back(num);
+            }
+        }
+        return ret;
+    }
+};
