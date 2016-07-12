3f8774ac93d994d4305a6f29047c15680201e5b9
wjhssfs <wjhssfs@gmail.com>
2016-07-11 19:21:07 -0700

    367 and 368

---
+++ 367.ValidPerfectSquare.hpp
@@ -0,0 +1,42 @@
+// 367. Valid Perfect Square
+// Given a positive integer num, write a function which returns True if num is a perfect square else False.
+// Note: Do not use any built-in library function such as sqrt.
+// Example 1:
+// Input: 16
+// Returns: True
+// Example 2:
+// Input: 14
+// Returns: False
+
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if (num < 0) return false;
+        long long l = 1, r = num;
+        while (l <=r) {
+            long long m = (l + r) / 2;
+            long long msq = m * m;
+            if (msq == num)
+                return true;
+            msq > num ? r = m - 1 : l = m + 1;
+        }
+        return false;
+    }
+};
+
+// A square number is 1+3+5+7+...,
+public boolean isPerfectSquare(int num) {
+    int i = 1;
+    while (num > 0) {
+        num -= i;
+        i += 2;
+    }
+    return num == 0;
+}
+
+// https://discuss.leetcode.com/topic/49342/3-4-short-lines-integer-newton-most-languages
+// https://en.wikipedia.org/wiki/Integer_square_root#Using_only_integer_division
+    long r = x;
+    while (r*r > x)
+        r = (r + x/r) / 2;
+    return r*r == x;
---
+++ 368LargestDivisibleSubset.hpp
@@ -0,0 +1,39 @@
+368. Largest Divisible Subset
+Given a set of distinct positive integers, find the largest subset such that every pair (Si, Sj)
+of elements in this subset satisfies: Si % Sj = 0 or Sj % Si = 0.
+If there are multiple solutions, return any subset is fine.
+Example 1:
+nums: [1,2,3]
+Result: [1,2] (of course, [1,3] will also be ok)
+Example 2:
+nums: [1,2,4,8]
+Result: [1,2,4,8]
+
+class Solution {
+public:
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        if (nums.empty()) return {};
+        sort(nums.begin(), nums.end());
+        vector<int> nSetSize(nums.size(), 1), pre(nums.size(), -1);
+        int maxSize = 1, maxEnd = 0;
+        for (int i = 1; i < nums.size(); ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[i] % nums[j] == 0 && nSetSize[i] < 1 + nSetSize[j]){
+                    nSetSize[i] = 1 + nSetSize[j];
+                    pre[i] = j;
+                    if (nSetSize[i] > maxSize) {
+                        maxSize = nSetSize[i];
+                        maxEnd = i;
+                    }
+                }
+            }
+        }
+        vector<int> ret(maxSize);
+        int cur = maxEnd;
+        for (int i = maxSize - 1; i >=0; --i) {
+            ret[i] = nums[cur];
+            cur = pre[cur];
+        }
+        return ret;
+    }
+};
