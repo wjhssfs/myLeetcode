2dae02d5917f2e788f337b68bdbadf1447563cbf
wjhssfs <wjhssfs@gmail.com>
2016-07-29 13:33:47 -0700

    update

--- 010RegularExpressionMatching.hpp
+++ 010RegularExpressionMatching.hpp
@@ -34,9 +34,8 @@ public:
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        if (p.empty())    return s.empty();
-
-        if ('*' == p[1])
+        if (p.empty()) return s.empty();
+        if (p.size() > 1 && '*' == p[1])
             // x* matches empty string or at least one character: x* -> xx*
             // *s is to ensure s is non-empty
             return (isMatch(s, p.substr(2)) || !s.empty() && (s[0] == p[0] || '.' == p[0]) && isMatch(s.substr(1), p));
