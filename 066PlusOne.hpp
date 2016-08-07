6d660b2e7e0a703084b601e929b29c81140e8e23
wjhssfs <wjhssfs@gmail.com>
2016-08-07 13:47:38 -0700

    update

--- 066PlusOne.hpp
+++ 066PlusOne.hpp
@@ -39,23 +39,3 @@ public:
         return digits;
     }
 };
-
-void plusone(vector<int> &digits)
-{
-    int n = digits.size();
-    for (int i = n - 1; i >= 0; --i)
-    {
-        if (digits[i] == 9)
-        {
-            digits[i] = 0;
-        }
-        else
-        {
-            digits[i]++;
-            return;
-        }
-    }
-        digits[0] =1;
-        digits.push_back(0);
-
-}
