1177615997d12c386a9dd8e33ce5e0c579827697
wjhssfs <wjhssfs@gmail.com>
2016-11-26 18:24:37 -0800

    452-455

---
+++ 454-4SumII.hpp
@@ -0,0 +1,33 @@
+// 454. 4Sum II
+// Given four lists A, B, C, D of integer values, compute how many tuples (i, j, k, l) there are such that A[i] + B[j] + C[k] + D[l] is zero.
+// To make problem a bit easier, all A, B, C, D have same length of N where 0 ≤ N ≤ 500. All integers are in the range of -228 to 228 - 1 and the result is guaranteed to be at most 231 - 1.
+// Example:
+// Input:
+// A = [ 1, 2]
+// B = [-2,-1]
+// C = [-1, 2]
+// D = [ 0, 2]
+// Output:
+// 2
+// Explanation:
+// The two tuples are:
+// 1. (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0
+
+class Solution {
+public:
+	int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
+		unordered_map<int, int> m(A.size() * B.size());
+		int total = 0;
+		for (auto a : A) {
+			for (auto b : B) {
+				++m[-(a + b)];
+			}
+		}
+		for (auto c : C) {
+			for (auto d : D) {
+				if (m[c + d]) total += m[c + d];
+			}
+		}
+		return total;
+	}
+};
