6769acce7cce08f73161c6d9505bdd4472f9c5e6
wjhssfs <wjhssfs@gmail.com>
2017-01-17 08:40:38 -0800

    upadtes

--- 369. Plus One Linked List.hpp
+++ 369. Plus One Linked List.hpp
@@ -1,4 +1,4 @@
-// 369. Plus One Linked List
+ // 369. Plus One Linked List
 // Given a non-negative integer represented as non-empty a singly linked list of digits, plus one to the integer.
 
 // You may assume the integer do not contain any leading zero, except the number 0 itself.
---
+++ 370. Range Addition.hpp
@@ -0,0 +1,56 @@
+// 370. Range Addition
+// Assume you have an array of length n initialized with all 0's and are given k update operations.
+
+// Each operation is represented as a triplet: [startIndex, endIndex, inc] which increments each element of subarray A[startIndex ... endIndex] (startIndex and endIndex inclusive) with inc.
+
+// Return the modified array after all k operations were executed.
+
+// Example:
+
+// Given:
+
+//     length = 5,
+//     updates = [
+//         [1,  3,  2],
+//         [2,  4,  3],
+//         [0,  2, -2]
+//     ]
+
+// Output:
+
+//     [-2, 0, 3, 5, 3]
+// Explanation:
+
+// Initial state:
+// [ 0, 0, 0, 0, 0 ]
+
+// After applying operation [1, 3, 2]:
+// [ 0, 2, 2, 2, 0 ]
+
+// After applying operation [2, 4, 3]:
+// [ 0, 2, 5, 5, 3 ]
+
+// After applying operation [0, 2, -2]:
+// [-2, 0, 3, 5, 3 ]
+// Hint:
+
+// Thinking of using advanced data structures? You are thinking it too complicated.
+// For each update operation, do you really need to update all elements between i and j?
+// Update only the first and end element is sufficient.
+// The optimal time complexity is O(k + n) and uses O(1) extra space.
+
+class Solution {
+public:
+    vector<int> getModifiedArray(int length, vector<vector<int>>& updates) {
+        vector<int> result(length + 1);
+        for (auto && update : updates) {
+            result[update[0]] += update[2];
+            result[update[1] + 1] -= update[2];
+        }
+        result.pop_back();
+        for (int i = 1; i < length; ++i) {
+            result[i] += result[i-1];
+        }
+        return result;
+    }
+};
