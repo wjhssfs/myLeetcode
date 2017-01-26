a8990df6be5a801e839182e00278355623e5545c
wjhssfs <wjhssfs@gmail.com>
2017-01-26 08:51:08 -0800

    updates

---
+++ 400/387. First Unique Character in a String.hpp
@@ -0,0 +1,40 @@
+// 387. First Unique Character in a String
+// Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+// Examples:
+
+// s = "leetcode"
+// return 0.
+
+// s = "loveleetcode",
+// return 2.
+// Note: You may assume the string contain only lowercase letters.
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int count[128] = {};
+        for (auto c : s) ++count[c];
+        for (int i = 0; i < s.size(); ++i) {
+            if (count[s[i]] == 1) return i;
+        }
+        return -1;
+    }
+};
+
+// if string is extremely long
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char, pair<int, int>> m;
+        int idx = s.size();
+        for(int i = 0; i < s.size(); i++) {
+            m[s[i]].first++;
+            m[s[i]].second = i;
+        }
+        for(auto &p : m) {
+            if(p.second.first == 1) idx = min(idx, p.second.second);
+        }
+        return idx == s.size() ? -1 : idx;
+    }
+};
