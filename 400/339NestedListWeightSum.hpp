16ca87e42d5c71e7abdeec46aa94f71e04446a8d
wjhssfs <wjhssfs@gmail.com>
2017-06-19 08:45:21 -0700

    update

--- 400/339NestedListWeightSum.hpp
+++ 400/339NestedListWeightSum.hpp
@@ -1,4 +1,4 @@
-// 341 Nested List Weight Sum
+// 339 Nested List Weight Sum
 // Given a nested list of integers, return the sum of all integers in the list weighted by their depth.
 // Each element is either an integer, or a list -- whose elements may also be integers or other lists.
 // Example 1:
