081aa7fc6fec7cd98087eef37cd32afef6d7f284
wjhssfs <wjhssfs@gmail.com>
2017-09-05 22:38:30 -0700

    update

--- 700/659. Split Array into Consecutive Subsequences.hpp
+++ 700/659. Split Array into Consecutive Subsequences.hpp
@@ -54,3 +54,27 @@ public:
         return true;
     }
 };
+
+// We iterate through the array once to get the frequency of all the elements in the array
+// We iterate through the array once more and for each element we either see if it can be appended to a previously constructed consecutive
+// sequence or if it can be the start of a new consecutive sequence. If neither are true, then we return false.
+class Solution {
+public:
+    bool isPossible(vector<int>& nums) {
+        unordered_map<int, int> freq, appendfreq;
+        for (auto i : nums) ++freq[i];
+        for (auto i : nums) {
+            if (freq[i] == 0) continue;
+            else if (appendfreq[i] > 0) {
+                --appendfreq[i];
+                ++appendfreq[i + 1];
+            } else if (freq[i + 1] > 0 && freq[i + 2] > 0) {
+                --freq[i + 1];
+                --freq[i + 2];
+                ++appendfreq[i + 3];
+            } else return false;
+            --freq[i];
+        }
+        return true;
+    }
+};
