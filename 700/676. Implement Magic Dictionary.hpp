15c82c3d2ce3b624dd5b34a9edfb0f61fd0d9b65
wjhssfs <wjhssfs@gmail.com>
2017-09-11 23:18:29 -0700

    updates

--- 700/676. Implement Magic Dictionary.hpp
+++ 700/676. Implement Magic Dictionary.hpp
@@ -70,3 +70,19 @@ public:
  * obj.buildDict(dict);
  * bool param_2 = obj.search(word);
  */
+
+
+class MagicDictionary(object):
+    def _candidates(self, word):
+        for i in xrange(len(word)):
+            yield word[:i] + '*' + word[i+1:]
+            
+    def buildDict(self, words):
+        self.words = set(words)
+        self.near = collections.Counter(cand for word in words
+                                        for cand in self._candidates(word))
+
+    def search(self, word):
+        return any(self.near[cand] > 1 or 
+                   self.near[cand] == 1 and word not in self.words
+                   for cand in self._candidates(word))
