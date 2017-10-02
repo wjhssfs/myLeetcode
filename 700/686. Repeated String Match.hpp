e7e4d32ed8e30cf6d3e5855d7b1c43bbd89be407
wjhssfs <wjhssfs@gmail.com>
2017-10-02 00:15:06 -0700

    update

---
+++ 700/686. Repeated String Match.hpp
@@ -0,0 +1,52 @@
+// 686. Repeated String Match
+
+// Given two strings A and B, find the minimum number of times A has to be repeated such that B is a substring of it. If no such solution, return -1.
+
+// For example, with A = "abcd" and B = "cdabcdab".
+
+// Return 3, because by repeating A three times (“abcdabcdabcd”), B is a substring of it; and B is not a substring of A repeated two times ("abcdabcd").
+
+// Note:
+// The length of A and B will be between 1 and 10000.
+
+class Solution {
+public:
+    int repeatedStringMatch(string A, string B) {
+        for(int i = 0; i < A.size(); ++i) {
+            int ii = i;
+            int n = 1;
+            for (int j = 0; j < B.size(); ++j) {
+                if (A[ii] != B[j]) break;
+                else {
+                    if (j == B.size() - 1) return n;
+                    ++ii;
+                    if (ii == A.size()) {
+                        ++n;
+                        ii = 0;
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+};
+
+class Solution {
+    public int repeatedStringMatch(String A, String B) {
+        StringBuilder sb = new StringBuilder();
+        int rep = 0;
+        while(sb.length() < B.length()){
+        	sb.append(A);
+        	rep++;
+        }
+        if(sb.toString().indexOf(B) >= 0){
+        	return rep;
+        }
+        sb.append(A);
+        rep++;
+        if(sb.toString().indexOf(B) >= 0){
+        	return rep;
+        }
+        return -1;
+    }
+}
---
+++ 700/689. Maximum Sum of 3 Non-Overlapping Subarrays.hpp
@@ -0,0 +1,73 @@
+// 689. Maximum Sum of 3 Non-Overlapping Subarrays
+// In a given array nums of positive integers, find three non-overlapping subarrays with maximum sum.
+
+// Each subarray will be of size k, and we want to maximize the sum of all 3*k entries.
+
+// Return the result as a list of indices representing the starting position of each interval (0-indexed). If there are multiple answers, return the lexicographically smallest one.
+
+// Example:
+// Input: [1,2,1,2,6,7,5,1], 2
+// Output: [0, 3, 5]
+// Explanation: Subarrays [1, 2], [2, 6], [7, 5] correspond to the starting indices [0, 3, 5].
+// We could have also taken [2, 1], but an answer of [1, 3, 5] would be lexicographically larger.
+// Note:
+// nums.length will be between 1 and 20000.
+// nums[i] will be between 1 and 65535.
+// k will be between 1 and floor(nums.length / 3).
+
+class Solution {
+public:
+	vector<int> maxSumOfThreeSubarrays(vector<int>& nums, int k) {
+		vector<int> kSum(nums.size() - k + 1);
+		for (int i = 0; i < k; ++i) kSum[0] += nums[i];
+		for (int i = 1; i < kSum.size(); ++i) kSum[i] = kSum[i - 1] + nums[k + i - 1] - nums[i - 1];
+		vector<vector<int>> p(2, vector<int>(sum.size(), -1)); // previous index
+		vector<int> dp = kSum;
+		for (int i = 0; i < 2; ++i) {
+			vector<int> newDp(sum.size(), 0);
+			int bestI = 0;
+			for (int j = k + k * i; j < kSum.size(); ++j) {
+				if (dp[j - k] > dp[bestI]) bestI = j - k;
+				newDp[j] = dp[bestI] + kSum[j];
+				p[i][j] = bestI;
+			}
+			swap(dp, newDp);
+		}
+		int i = max_element(dp.begin(), dp.end()) - dp.begin();
+		return { p[0][p[1][i]], p[1][i], i};
+	}
+};
+
+class Solution {
+public:
+	vector<int> maxSumOfThreeSubarrays(vector<int>& nums, int k) {
+		vector<int> r;
+		vector<int> sum(nums.size() - k + 1);
+		int maxSum = 0;
+		for (int i = 0; i < k; ++i) sum[0] += nums[i];
+		for (int i = 1; i < sum.size(); ++i) {
+			sum[i] = sum[i - 1] + nums[k + i - 1] - nums[i - 1];
+		}
+
+		vector<int> maxSumAfter(sum.size(), sum.size() - 1);
+		for (int i = maxSumAfter.size() - 2; i >= 0; --i) {
+			if (sum[i] < sum[maxSumAfter[i + 1]]) {
+				maxSumAfter[i] = maxSumAfter[i + 1];
+			}
+			else {
+				maxSumAfter[i] = i;
+			}
+		}
+
+		for (int i = 0; i < sum.size(); ++i) {
+			for (int j = i + k; j + k < sum.size(); ++j) {
+				int curSum = sum[i] + sum[j] + sum[maxSumAfter[j + k]];
+				if (curSum > maxSum) {
+					maxSum = curSum;
+					r = { i, j, maxSumAfter[j + k] };
+				}
+			}
+		}
+		return r;
+	}
+};
