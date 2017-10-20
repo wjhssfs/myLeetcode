7ea76841010236a5a383ef76f7f9030efec30938
wjhssfs <wjhssfs@gmail.com>
2017-10-19 22:43:48 -0700

    updates

--- 600/531. Lonely Pixel I.hpp
+++ 600/531. Lonely Pixel I.hpp
@@ -38,3 +38,22 @@ public:
         return m.size();
     }
 };
+
+public int findLonelyPixel(char[][] picture) {
+    int n = picture.length, m = picture[0].length;
+    
+    int[] rowCount = new int[n], colCount = new int[m];
+    for (int i=0;i<n;i++) 
+        for (int j=0;j<m;j++) 
+            if (picture[i][j] == 'B') { rowCount[i]++; colCount[j]++; }
+
+    int count = 0;
+    for (int i=0;i<n;i++) 
+        for (int j=0;j<m;j++) 
+            if (picture[i][j] == 'B' && rowCount[i] == 1 && colCount[j] == 1) count++;
+                
+    return count;
+}
+
+def findLonelyPixel(self, picture):
+    return sum(col.count('B') == 1 == picture[col.index('B')].count('B') for col in zip(*picture))
