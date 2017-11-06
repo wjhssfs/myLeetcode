5305a29f1315f13f7a78bfc1bed789e4e8800576
wjhssfs <wjhssfs@gmail.com>
2017-11-05 21:26:56 -0800

    updates

--- 600/522. Longest Uncommon Subsequence II.hpp
+++ 600/522. Longest Uncommon Subsequence II.hpp
@@ -43,3 +43,12 @@ private:
         return true;
     }
 };
+
+def findLUSlength(self, strs):
+    def issubsequence(s, t):
+        t = iter(t)
+        return all(c in t for c in s)
+    for s in sorted(strs, key=len, reverse=True):
+        if sum(issubsequence(s, t) for t in strs) == 1:
+            return len(s)
+    return -1
