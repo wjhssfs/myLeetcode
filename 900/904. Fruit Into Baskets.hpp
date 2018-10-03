f5699a5671f59968e66d4cb3df994863fa84340e
wjhssfs <wjhssfs@gmail.com>
2018-10-03 00:37:46 -0700

    update

--- 900/904. Fruit Into Baskets.hpp
+++ 900/904. Fruit Into Baskets.hpp
@@ -60,3 +60,15 @@ public:
         return res;
     }
 };
+
+// https://leetcode.com/problems/fruit-into-baskets/discuss/170745/Problem:-Longest-Subarray-With-2-Elements
+int totalFruit(vector<int> tree) {
+        int res = 0, cur = 0, count_b = 0, a = 0, b = 0;
+        for (int c :  tree) {
+            cur = c == a || c == b ? cur + 1 : count_b + 1;
+            count_b = c == b ? count_b + 1 : 1;
+            if (b != c) a = b, b = c;
+            res = max(res, cur);
+        }
+        return res;
+    }
